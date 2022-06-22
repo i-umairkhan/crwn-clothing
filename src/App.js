1e16bc58c293214caa62b74ab8a6008ebfcee705
umair2607 <umair.k2607@gmail.com>
2022-06-22 21:18:17 +0500

    project clean up and basic scafolding

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
 const App = () => {
-  return <div>Hello World</div>;
+  const catagories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {catagories.map(({ title, imageUrl }) => (
+        <div className="category-container">
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
