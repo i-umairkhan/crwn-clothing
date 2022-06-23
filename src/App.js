4f33167fa850e6982bf171eeb0443ee133934052
umair2607 <umair.k2607@gmail.com>
2022-06-24 01:39:43 +0500

    sign-in Navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Home from "./Routes/Home/Home.Component.jsx";
 import { Routes, Route } from "react-router-dom";
+import Home from "./Routes/Home/Home.Component.jsx";
 import Navigation from "./Routes/Navigation/Navigation.Component.jsx";
+import SignIn from "./Routes/Sign-In/Sign-In.Component.jsx";
 
 const Shop = () => {
   return <h1>I am Shop</h1>;
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index={true} element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
