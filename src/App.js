e624d68735aaf7275244754db85f3eb1acac1676
umair2607 <umair.k2607@gmail.com>
2022-06-23 23:16:02 +0500

    Basic Navigation Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Home from "./Routes/Home/Home.Component.jsx";
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./Routes/Navigation/Navigation.Component.jsx";
+
+const Shop = () => {
+  return <h1>I am Shop</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index={true} element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
