f94063cb40a0da8210edfca9619602c226c91059
umair2607 <umair.k2607@gmail.com>
2022-06-28 03:12:17 +0500

    added shop card

---
+++ src/Components/Product-Card/Product-Card.Component.jsx
@@ -0,0 +1,18 @@
+import "./Product-Card.Styles.scss";
+import Button from "../Button/Button.Component.jsx";
+
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
+  return (
+    <div className="product-card-container">
+      <img src={imageUrl} alt={name} />
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <Button buttonType="inverted">Add to card</Button>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/Routes/Shop/Shop.Component.jsx
+++ src/Routes/Shop/Shop.Component.jsx
@@ -1,14 +1,14 @@
 import { useContext } from "react";
 import { ProductsContext } from "../../Contexts/Products.Context.jsx";
+import ProductCard from "../../Components/Product-Card/Product-Card.Component.jsx";
+import "./Shop.Styles.scss";
 
 const Shop = () => {
   const { products } = useContext(ProductsContext);
   return (
-    <div>
-      {products.map(({ id, name }) => (
-        <div key={id}>
-          <h1>{name}</h1>
-        </div>
+    <div className="products-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   );
