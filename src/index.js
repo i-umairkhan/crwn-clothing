7b83400a568161635fc9b36267495df7ce29f74c
umair2607 <umair.k2607@gmail.com>
2022-06-28 02:34:05 +0500

    implemeted products context

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
 import { UserProvider } from "./Contexts/User.Context.jsx";
+import { ProductsProvider } from "./Contexts/Products.Context.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
